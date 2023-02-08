73bb5f200643f99f10df3a968ea379de97f61293
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2023-02-08 22:26:23 +0530

    complete crud operations

---
+++ main.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI, status, Response, HTTPException
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
+
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    # id: int
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [
+    {"title": "Hello World", "content": "This is my first post", "id": 1},
+    {"title": "Hello World 2", "content": "This is my second post", "id": 2},
+]
+
+
+def find_post(post_id: int):
+    for post in my_posts:
+        if post["id"] == post_id:
+            return post
+    return None
+
+
+def find_post_index(post_id: int):
+    for i, post in enumerate(my_posts):
+        if post["id"] == post_id:
+            return i
+    return None
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/posts")
+def get_posts():
+    return my_posts
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return post_dict
+
+
+@app.get("/posts/{post_id}")
+def get_post(post_id: int):
+    post = find_post(post_id)
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+        )
+    return post
+
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int):
+    post = find_post(post_id)
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+        )
+    my_posts.remove(post)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{post_id}")
+def update_post(post_id: int, post: Post):
+    post_index = find_post_index(post_id)
+    if post_index is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+        )
+    post_dict = post.dict()
+    post_dict["id"] = post_id
+    my_posts[post_index] = post_dict
+    return post_dict
+
+
+# uvicorn main:app --reload
