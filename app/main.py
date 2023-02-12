7ae31c97162bef8ff324406f7c939f6351757957
adityapunetha <adityapunetha6@gmailcom>
2023-02-12 15:52:52 +0530

    sqlalchemy models

--- app/main.py
+++ app/main.py
@@ -8,21 +8,14 @@ from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
 from . import models
-from .database import SessionLocal, engine
+from .database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 class Post(BaseModel):
-    # id: int
     title: str
     content: str
     published: bool = True
--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
-from .database import Base, engine
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+from .database import Base, engine
 
 
 class Post(Base):
@@ -7,4 +10,7 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default="TRUE", nullable=False)
+    creeated_at = Column(
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
+    )
