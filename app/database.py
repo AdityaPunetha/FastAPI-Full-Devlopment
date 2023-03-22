8c878075537254eda07f22f1a62e649559651afb
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2023-03-22 15:28:33 +0530

    alembic init

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from app.config import settings
 
 # import psycopg2
 # from psycopg2.extras import RealDictCursor
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
