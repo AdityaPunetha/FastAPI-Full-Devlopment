d2f4fe3ce3b1b553ccd1a658eabc4075b6563e0a
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2023-03-22 16:29:44 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app import models
 from app.database import engine
 from app.routers import post, user, auth, vote
@@ -8,6 +9,21 @@ from app.routers import post, user, auth, vote
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8000",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
