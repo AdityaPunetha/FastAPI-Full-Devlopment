a74aff00e96ec70f4e5b797f0c8b1365ee7ff299
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2023-03-25 11:55:18 +0530

    login tests

---
+++ tests/conftest.py
@@ -0,0 +1,38 @@
+from fastapi.testclient import TestClient
+import pytest
+from app.main import app
+from app import models
+from app.config import settings
+from app.database import get_db
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture()
+def session():
+    models.Base.metadata.drop_all(bind=engine)
+    models.Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,17 @@
+import pytest
+from jose import jwt
 from app import schemas
-from tests.database import client, session
+from app.config import settings
+
+
+@pytest.fixture()
+def test_user(client):
+    user_data = {"email": "create_user@test.com", "password": "create_user_test"}
+    response = client.post("/user/", json=user_data)
+    assert response.status_code == 201
+    new_user = response.json()
+    new_user["password"] = user_data["password"]
+    return new_user
 
 
 def test_root(client):
@@ -15,3 +27,18 @@ def test_create_user(client):
     new_user = schemas.UserOut(**response.json())
     assert response.status_code == 201
     assert new_user.email == "test@test.com"
+
+
+def test_login_user(client, test_user):
+    response = client.post(
+        "/login",
+        data={"username": test_user["email"], "password": test_user["password"]},
+    )
+    login_res = schemas.Token(**response.json())
+    payload = jwt.decode(
+        login_res.access_token, settings.secret_key, algorithms=[settings.algorithm]
+    )
+    user_id: str = payload.get("user_id")
+    assert user_id == test_user["id"]
+    assert login_res.token_type == "bearer"
+    assert response.status_code == 200
